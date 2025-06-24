b55f66526bba396836b7f67eba0dc6f2b1a8b762
Marciel-Vinicius <marcielviniciusdelara@gmail.com>
2025-06-24 16:32:14 -0300

    alterado nome do arquivo

--- backend/src/routes/chamados.routes.js
+++ backend/src/routes/chamados.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-import { authenticate } from '../middleware/auth.js';
+import { authenticate } from '../middlewares/auth.js';
 
 const router = express.Router();
 const prisma = new PrismaClient();
